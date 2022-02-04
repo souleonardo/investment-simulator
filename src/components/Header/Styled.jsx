97b748999f1f6764c7a903f7fbfa794b109267f0
Leonardo <leonardomibsp@hotmail.com>
2022-02-03 21:14:36 -0300

    initializing project with styled-components

---
+++ src/components/Header/Styled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components"
+
+const HeaderStyle = styled.div`
+  font-size: 14px;
+  text-align: center;
+  padding: 10px 0px;
+`;
+
+export default HeaderStyle
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HeaderStyle from './Styled';
+
+function Header() {
+  return(
+    <HeaderStyle><h1>Simulador de Investimentos</h1></HeaderStyle> 
+  )  
+}
+
+export default Header;
